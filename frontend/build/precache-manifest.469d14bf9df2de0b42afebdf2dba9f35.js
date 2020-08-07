6608fb4411c20f9dfc6c2fb33c96b16e184ec893
tedhoon <gt0305@likelion.org>
2020-08-07 11:31:29 +0900

    [dp] add basket test dp

---
+++ frontend/build/precache-manifest.469d14bf9df2de0b42afebdf2dba9f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a1194d03801a9f8efaf5a86d5781fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4eed7a8a8985d0e9e3e",
+    "url": "/static/css/main.093cb480.chunk.css"
+  },
+  {
+    "revision": "af374cb875f5799963fd",
+    "url": "/static/js/2.29550fc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29550fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4eed7a8a8985d0e9e3e",
+    "url": "/static/js/main.4b1cd762.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
