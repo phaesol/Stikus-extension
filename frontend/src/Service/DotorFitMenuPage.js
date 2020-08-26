e0dd84ff1aad10dab8767bfb778214b0141a978c
tedhoon <gt0305@likelion.org>
2020-08-26 16:39:28 +0900

    [add] MenuPage 버튼 스타일링

--- frontend/src/Service/DotorFitMenuPage.js
+++ frontend/src/Service/DotorFitMenuPage.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 import IdCard from '../Components/Useful/IdCard';
 import { useHistory } from 'react-router-dom';
+import MAKE_NUTRITION_IMAGE from '../Images/Basic/menu-to-nutrient.png';
+
 
 function DoctorFitMenuPage () {
     const history = useHistory();
@@ -12,14 +14,22 @@ function DoctorFitMenuPage () {
     if (!which) 
         return (
             <>
-                <MainInfo>내 아이에게 &nbsp;
-                    <InnerInfo>딱! 맞는 제품</InnerInfo>은?
-                </MainInfo>
+                <StyledMainInfo>내 아이에게 &nbsp;
+                    <StyledInnerInfo>딱! 맞는 제품</StyledInnerInfo>은?
+                </StyledMainInfo>
                         
-                <SubInfo>내 아이만을 위한 맞춤정보와 제품을 만들 수 있어요<br />이미 5,352명의 아이들이 이용했어요</SubInfo>
+                <StyledSubInfo>내 아이만을 위한 맞춤정보와 제품을 만들 수 있어요<br />이미 5,352명의 아이들이 이용했어요</StyledSubInfo>
 
                 <IdCard />
-                <button onClick={goToMakeNutrinet} />
+                
+                <StyledNutrientFitMenu onClick={goToMakeNutrinet}>
+                    <StyledMenuDescWrapper>
+                        <StyledInnerMenuDesc>맞춤 영양제                        
+                            <StyledInnerMenuDescSmall>만들기</StyledInnerMenuDescSmall>
+                        </StyledInnerMenuDesc>
+                    </StyledMenuDescWrapper>
+                
+                </StyledNutrientFitMenu>
             </>
         )
     if (which === "make-nutrient") 
@@ -35,7 +45,7 @@ function DoctorFitMenuPage () {
 export default DoctorFitMenuPage;
 
 
-const MainInfo = styled.div`
+const StyledMainInfo = styled.div`
     margin: 25px 0;
     font-size: 28px;
     font-weight: 300;
@@ -43,17 +53,48 @@ const MainInfo = styled.div`
     letter-spacing: -1.4px;
 `;
 
-const InnerInfo = styled.div`
+const StyledInnerInfo = styled.div`
     display: inline;
     font-weight: 700;
-    
     color: #e16a49;
 `;
 
-const SubInfo = styled.div`
+const StyledSubInfo = styled.div`
     font-size: 15px;
     color: #080808;
     letter-spacing: -0.75px;
     line-height: 1.47;
     margin: 15px 0 30px;
+`;
+
+// menu
+const StyledNutrientFitMenu = styled.div`
+    position: relative;
+    margin-top: 20px;
+    width: 100%;
+    height: 127px;
+    background: round;
+    background-image: url(${MAKE_NUTRITION_IMAGE});
+    background-color: #d0dbfc;
+    border-radius: 10px;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+    display: flex;
+    align-items: center;
+`;
+
+const StyledMenuDescWrapper = styled.div`
+    margin-left: 20%;
+`;
+
+const StyledInnerMenuDesc = styled.div`
+    letter-spacing: -1.4px;
+    color: #2b428e;
+    font-size: 25px;
+    font-weight: 600;
+    letter-spacing: -1.25px;
+`;
+
+const StyledInnerMenuDescSmall = styled.div`
+    font-size: 20px;
+    font-weight: 400;
 `;
