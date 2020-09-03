f084ea5d4d1b77517d229aefcc6026906248b2f8
chichi <jang.chihun@gmail.com>
2020-09-03 10:25:28 +0900

    [mdf] bottom next page button 분리 및 path로 경로 전달시 withRouter이용해서 routing시켜줌

---
+++ frontend/src/Components/button/StyledNextButton.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import React, { useState, useCallback } from "react";
+import { withRouter } from "react-router-dom";
+
+const StyledButton = styled.button`
+  font-size: 17px;
+  width: 100%;
+  height: 45px;
+  background: #2b428e;
+  border-radius: 5px;
+  letter-spacing: -0.9px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+const StyledNextButton = ({ children, path, history }) => {
+  function handlePath() {
+    console.log(path);
+    history.push(path);
+  }
+
+  return <StyledButton onClick={handlePath}>{children}</StyledButton>;
+};
+
+export default withRouter(StyledNextButton);
