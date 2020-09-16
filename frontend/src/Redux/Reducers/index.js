b5c0111884eb030c5ea8884090cb27c3ba69170d
chichi <jang.chihun@gmail.com>
2020-09-16 15:15:46 +0900

    [add] recommendFit redux apply

--- frontend/src/Redux/Reducers/index.js
+++ frontend/src/Redux/Reducers/index.js
@@ -1,8 +1,9 @@
-import { combineReducers } from 'redux';
-import petInfoReducer from './petInfoReducer';
-
+import { combineReducers } from "redux";
+import petInfoReducer from "./petInfoReducer";
+import recommendFit from "../../containers/nutrientFit/module/recommendFit";
 const rootReducer = combineReducers({
-    petInfo: petInfoReducer,
+  petInfo: petInfoReducer,
+  recommendFit,
 });
 
 export default rootReducer;
---
+++ frontend/src/containers/nutrientFit/module/nouse-index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import recommendFit from "./recommendFit";
+
+const recommendReducer = combineReducers({
+  recommendFit,
+});
+
+export default rootReducer;
+
+//duck type과 기존 type혼재
