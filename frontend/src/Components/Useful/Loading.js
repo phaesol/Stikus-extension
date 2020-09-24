fa23be1b97c870954e4e0ce10adcadd7bedb5bf6
chichi <jang.chihun@gmail.com>
2020-09-24 11:11:24 +0900

    [mdf] change loading component material UI circle process

--- frontend/src/Components/Useful/Loading.js
+++ frontend/src/Components/Useful/Loading.js
@@ -1,17 +1,67 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+
+function CircularProgressWithLabel(props) {
+  return (
+    <Box position="relative" display="inline-flex">
+      <StyledCircularProgress size={200} variant="static" {...props} />
+      <Box
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <StyledTypography
+          variant="caption"
+          component="div"
+          color="textSecondary"
+        >{`${Math.round(props.value)}%`}</StyledTypography>
+      </Box>
+      <Box
+        top={0}
+        left={0}
+        bottom={-350}
+        right={0}
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <StyledWaitMsg>잠시만 기다려 주세요</StyledWaitMsg>
+      </Box>
+    </Box>
+  );
+}
 
 function Loading() {
   const [progress, setProgress] = useState(0);
   // 현재 1초 로딩용
-  useEffect(() => {
-    if (progress === 99) {
-      return;
-    }
-    setTimeout(() => {
-      setProgress(progress + 1);
-    }, 10);
-  }, [progress]);
+  // useEffect(() => {
+  //   if (progress === 99) {
+  //     return;
+  //   }
+  //   setTimeout(() => {
+  //     setProgress(progress + 1);
+  //   }, 10);
+  // }, [progress]);
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 100 : prevProgress + 9
+      );
+    }, 100);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <>
@@ -23,10 +73,7 @@ function Loading() {
       </StyledSubInfo>
 
       <StyledLoaderWrapper>
-        <StyledLoadingSpinner></StyledLoadingSpinner>
-        <StyledProgressNum>{progress}</StyledProgressNum>
-        <StyledPercent>%</StyledPercent>
-        <StyledLoaderInfo>잠시만 기다려 주세요</StyledLoaderInfo>
+        <CircularProgressWithLabel value={progress} />
       </StyledLoaderWrapper>
     </>
   );
@@ -68,65 +115,26 @@ const StyledLoaderWrapper = styled.div`
   justify-content: center;
 `;
 
-const KeyframesSpin = keyframes`
-    0% {
-        -webkit-transform: rotate(0deg);
-        -moz-transform: rotate(0deg);
-        -ms-transform: rotate(0deg);
-        -o-transform: rotate(0deg);
-        transform: rotate(0deg);
-    }
-    100% {
-        -webkit-transform: rotate(360deg);
-        -moz-transform: rotate(360deg);
-        -ms-transform: rotate(360deg);
-        -o-transform: rotate(360deg);
-        transform: rotate(360deg);
-    }
-`;
+const StyledCircularProgress = withStyles({
+  colorPrimary: {
+    color: "#E16A49",
+  },
+  svg: {
+    strokeLinecap: "round",
+  },
+})(CircularProgress);
 
-const StyledLoadingSpinner = styled.div`
-  -webkit-animation-duration: 0.7s;
-  -moz-animation-duration: 0.7s;
-  animation-duration: 0.7s;
-  -webkit-animation-iteration-count: 2;
-  -moz-animation-iteration-count: 2;
-  animation-iteration-count: 2;
-  -webkit-animation-name: ${KeyframesSpin};
-  -moz-animation-name: ${KeyframesSpin};
-  animation-name: ${KeyframesSpin};
-  -webkit-animation-timing-function: linear;
-  -moz-animation-timing-function: linear;
-  animation-timing-function: linear;
-  height: 150px;
-  width: 150px;
-  border: 18px solid #e16a49;
-  border-right-color: transparent;
-  border-radius: 50%;
-`;
+const StyledTypography = withStyles({
+  caption: {
+    fontSize: "48px",
+    color: "#333333",
+  },
+})(Typography);
 
-const StyledProgressNum = styled.span`
-  position: relative;
-  top: -132px;
-  font-size: 55px;
-  font-weight: 700;
-  color: #2c3e50;
-`;
-
-const StyledPercent = styled.span`
-  position: relative;
-  top: -170px;
-  left: 42px;
-  font-size: 23px;
-  font-weight: 700;
-  color: #2c3e50;
-`;
-
-const StyledLoaderInfo = styled.div`
-  position: relative;
-  top: -90px;
-  font-size: 15px;
+const StyledWaitMsg = styled.div`
+  text-align: center;
+  font: normal normal 300 22px/33px Noto Sans KR;
+  letter-spacing: -1.1px;
   color: #080808;
-  letter-spacing: -0.75px;
-  line-height: 1.47;
+  opacity: 1;
 `;
