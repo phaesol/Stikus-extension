d9b8b156711a88a2a9cbfeb2cf91dfed45b09162
chichi <jang.chihun@gmail.com>
2020-10-22 16:53:33 +0900

    [ADD] preview modal에 넘겨줄 container 연결...

--- frontend/src/containers/PaymentPageContainer.js
+++ frontend/src/containers/PaymentPageContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import PaymentPage from "../Service/NutrientFit/PaymentPage";
-const PaymentPageContainer = ({ choosecards, materialList, petName }) => {
+const PaymentPageContainer = ({
+  choosecards,
+  materialList,
+  petName,
+  remove_duplicate_materiale,
+  final_order_nutrient,
+}) => {
   return (
     <PaymentPage
       choosecards={choosecards}
-      materialList={materialList}
+      final_mateiral={
+        final_order_nutrient ? final_order_nutrient : remove_duplicate_materiale
+      }
       petName={petName}
     />
   );
@@ -16,6 +24,8 @@ export default connect(
     petName: state.petInfo.name,
     choosecards: state.recommendFit.choosecards,
     materialList: state.resultMaterial.materialList,
+    remove_duplicate_material: state.resultMaterial.remove_duplicate_material,
+    final_order_nutrient: state.selfMake.final_order_nutrient,
   }),
   {}
 )(PaymentPageContainer);
