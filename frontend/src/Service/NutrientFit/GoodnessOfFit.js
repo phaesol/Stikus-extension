5b541d4ed9ae06ac15e4925fe3316661a507f4bb
chichi <jang.chihun@gmail.com>
2020-11-21 13:42:06 +0900

    [ADD] 원료 적합도에서 경고 재료 있을때 재확인 모달창 추가

--- frontend/src/Service/NutrientFit/GoodnessOfFit.js
+++ frontend/src/Service/NutrientFit/GoodnessOfFit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { withRouter } from "react-router-dom";
@@ -16,9 +16,31 @@ const GoodnessOfFit = ({
 }) => {
   console.log("확인해보자아아아아아아아", check_final_order_nutrient);
   const fitscore = 5 - caution_nutrient.length;
-
+  const [visible, setVisible] = useState(false);
   return (
-    <div>
+    <>
+      {visible && (
+        <>
+          <BlackBg></BlackBg>
+          <CautionModal>
+            <p>
+              추천하지 않는 원료가
+              <br /> 포함되어있습니다.
+              <br /> 그래도{" "}
+              {caution_nutrient.map((item) => (
+                <span>{item}</span>
+              ))}
+              를 선택하시겠습니까?
+            </p>
+            <StyledButtonWrapper>
+              <StyledPrevButton to={"/payment-page"}>만들기</StyledPrevButton>
+              <StyledReButton onClick={() => setVisible(false)}>
+                다시 선택
+              </StyledReButton>
+            </StyledButtonWrapper>
+          </CautionModal>
+        </>
+      )}
       <StyledFitHeader>
         <p>
           선택하신 원료들은
@@ -58,11 +80,17 @@ const GoodnessOfFit = ({
         <StyledBackBtn onClick={() => history.goBack()}>
           다시 선택
         </StyledBackBtn>
-        <StyledNextButton path={"/payment-page"}>
-          그대로 만들기
-        </StyledNextButton>
+        {caution_nutrient.length >= 1 ? (
+          <StyledReButton onClick={() => setVisible(true)}>
+            그대로 만들기
+          </StyledReButton>
+        ) : (
+          <StyledNextButton to={"/payment-page"}>
+            그대로 만들기
+          </StyledNextButton>
+        )}
       </StyledButtonWrapper>
-    </div>
+    </>
   );
 };
 
@@ -153,4 +181,55 @@ const StyledBackBtn = styled.button`
     color: #3854b0;
     background-color: #c9c9c9;
   }
+`;
+
+const BlackBg = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background-color: #080808;
+  opacity: 0.4;
+  z-index: 10;
+`;
+
+const CautionModal = styled.div`
+  width: 320px;
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 10px;
+  opacity: 1;
+  padding: 15px;
+  box-sizing: border-box;
+  color: #333333;
+  margin-top: 100px;
+  position: fixed;
+  left: calc(50% - 160px);
+  top: 0;
+  background-color: white;
+  & > p {
+    text-align: center;
+    padding: 30px 0;
+  }
+  & > p > span {
+    color: #e16a49;
+  }
+  z-index: 15;
+  & span + span:before {
+    content: "\\2C";
+    padding-right: 5px;
+  }
+`;
+
+const StyledReButton = styled.button`
+  border: none;
+  background: none;
+  font-size: 17px;
+  width: 100%;
+  height: 45px;
+  background: #2b428e;
+  border-radius: 5px;
+  letter-spacing: -0.9px;
+  color: #ffffff;
+  cursor: pointer;
 `;
