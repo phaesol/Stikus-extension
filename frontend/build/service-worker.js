02832e43d671d70e25ac2f1204151d2c96509c7d
tedhoon <gt0305@likelion.org>
2021-01-05 15:12:32 +0900

    [dp] deploy

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a38d007dde482cf37ccc1ef33c79860.js"
+  "/precache-manifest.de28d3a829d7396d5caeff916dfa1422.js"
 );
 
 self.addEventListener('message', (event) => {
